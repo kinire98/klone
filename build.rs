775ea5446b0873ac3fe97ba8f20f01a6c9d4fd9c
kinire98 <iknitgarri@outlook.com>
2024-03-13 10:57:04 +0100

    Litte change to build.rs

--- build.rs
+++ build.rs
@@ -8,6 +8,9 @@ use std::process::Command;
 #[cfg(windows)]
 fn main() {
     let program_data = PathBuf::from("C:\\ProgramData\\klone");
+    if program_data.is_dir() {
+        return;
+    }
     let _ = fs::create_dir_all(&program_data);
     let _ = fs::write(program_data.join("exclusion.json"), "[]");
     let _ = fs::write(

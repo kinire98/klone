6ffda77364eb8c7f0c22d3492f74f10b18692fae
kinire98 <iknitgarri@outlook.com>
2024-04-08 13:29:44 +0200

    Added foundation for threadpool

--- src/app/initial_copy.rs
+++ src/app/initial_copy.rs
@@ -4,13 +4,17 @@ use crate::output::cli;
 use std::fs::DirEntry;
 use std::path::PathBuf;
 use std::sync::mpsc::{self, Receiver, Sender};
+use threads_pool::ThreadPool;
+
+static POOL: ThreadPool = ThreadPool::build(num_cpus::get() / 2);
 
 pub fn initial_copy(origin_dir: PathBuf, mut target_dir: PathBuf) -> Result<(), Error> {
     target_dir.push(origin_dir.iter().next_back().unwrap());
     let (tx, rx): (Sender<String>, Receiver<String>) = mpsc::channel();
     let _wrap = super::wrapper::Wrapper;
     cli(rx)?;
-    start_initial_copy(origin_dir, target_dir, tx)
+    start_initial_copy(origin_dir, target_dir, tx)?;
+    Ok(())
 }
 fn start_initial_copy(
     origin_dir: PathBuf,
@@ -47,16 +51,18 @@ fn copy_operations(
         return start_initial_copy(origin_dir.path(), target_dir, tx);
     }
     std::fs::create_dir_all(&path_target_dir).unwrap();
-    fs_extra::file::copy(
-        origin_dir.path(),
-        target_dir,
-        &fs_extra::file::CopyOptions {
-            overwrite: true,
-            ..Default::default()
-        },
-    )
-    .map_err(|_| Error {
-        kind: ErrorKind::FSError,
-    })?;
+    POOL.execute(move || {
+        fs_extra::file::copy(
+            origin_dir.path(),
+            target_dir,
+            &fs_extra::file::CopyOptions {
+                overwrite: true,
+                ..Default::default()
+            },
+        )
+        .map_err(|_| Error {
+            kind: ErrorKind::FSError,
+        });
+    });
     Ok(())
 }
